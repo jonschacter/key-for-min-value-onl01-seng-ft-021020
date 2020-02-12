b83e5ed9dbdfde4830817c4e43153e760acc249a
Jon Schacter <schacter.jon@gmail.com>
2020-02-12 19:52:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,3 +1,11 @@
 def key_for_min_value(list)
+  lowest_value = nil
+  lowest_key = nil
 
+  list.each do |key, value|
+    if lowest_value == nil || value < lowest_value
+      lowest_value = value
+      lowest_key = key
+    end
+  end
 end
